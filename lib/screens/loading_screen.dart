5b3bddb351d0dbcf28c7042dceee741ba65613f2
NdegwaJulius <juliusndegwa2@gmail.com>
2022-10-02 23:49:47 +0300

    Stateful Widget Lifecycle  - Calling  Geolocation in the initState method in Async

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -2,31 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 
 class LoadingScreen extends StatefulWidget {
-  const LoadingScreen({Key? key}) : super(key: key);
+
   @override
-  State<LoadingScreen> createState() => _LoadingScreenState();
+  _LoadingScreenState createState() => _LoadingScreenState();
 }
 
-class _LoadingScreenState extends State<LoadingScreen> {
 
-  Future<void> getLocation() async {
-    LocationPermission permission = await Geolocator.requestPermission();
-    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
+
+  void getLocation() async {
+    //LocationPermission permission = await Geolocator()
+    Position position =  await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
     print(position);
 
 }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            //Get the current location
-            getLocation();
-          },
-          child: const Text('Get Location'),
-        ),
-      ),
-    );
+    print('biuld called');
+    return Scaffold();
   }
 }
