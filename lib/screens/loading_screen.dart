aa59c5ff696fae2e67839b9b6d47964acd7c8792
NdegwaJulius <juliusndegwa2@gmail.com>
2022-10-05 09:36:52 +0300

    Adding a spinnner while the user waits

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:weather_app/services/networking.dart';
-
+import 'location_screen.dart';
 
 class LoadingScreen extends StatefulWidget {
 
@@ -33,6 +34,9 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
       NetworkHelper networkHelper = NetworkHelper('https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=bc60b25202029239c778f7703abb735d');
       var weatherData = await networkHelper.getData();
+      Navigator.push(context, MaterialPageRoute(builder: (context){
+        return LocationScreen();
+      }));
 
 
 
@@ -49,7 +53,14 @@ class _LoadingScreenState extends State<LoadingScreen> {
   @override
   Widget build(BuildContext context) {
 
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: SpinKitFadingFour(
+          color: Colors.white,
+          size: 100.0,
+        ),
+      ),
+    );
   }
 
 }
